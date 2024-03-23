c229bad5ce9a6312276dda31fb4141a602d4358a
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-03-23 12:42:52 +0530

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
-import "./app.scss";
+import "./App.scss";
 
 const App = () => {
   return (

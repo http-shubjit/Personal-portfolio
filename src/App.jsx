184e0b3c493e672883dd12ca4f4971af1efe8fa2
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-03-23 12:39:43 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
@@ -6,6 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
+import "./app.scss";
 
 const App = () => {
   return (

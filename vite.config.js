855bbca0b6b435f18053757ec49d4e1fe46a36e4
MGT69 <95029429+http-shubjit@users.noreply.github.com>
2024-03-23 12:31:55 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import scss from "vite-plugin-scss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), scss()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ["framer-motion", "@emailjs/browser"],
